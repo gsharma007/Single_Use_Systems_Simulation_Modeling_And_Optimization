bec6b21fca87853ab4111c5719598f457e44ac3b
Gaurav Sharma <sharmagaurav459@gmail.com>
2019-10-16 01:22:07 -0700

    Add files via upload

---
+++ Bio_Mfg_Versions/Bio_Mfg_17.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 14 23:40:08 2019
+
+@author: gauravsharma
+"""
+
+#import pandas as pd
+
+for compa in range(0,50):    
+    exec(open("Bio_Mfg_15.py").read())
+    print("iteration number",compa)
+    
+#df_replication = pd.DataFrame()
