09d7f9ce78f062ec4f2992379ea1ab605fefaffe
Gaurav Sharma <sharmagaurav459@gmail.com>
2019-10-25 14:23:56 -0700

    Add files via upload

--- Bio_Mfg_Versions/Bio_Mfg_Run_File.py
+++ Bio_Mfg_Versions/Bio_Mfg_Run_File.py
@@ -12,6 +12,7 @@ from Bio_mfg_19 import function_2
 import seaborn as sns
 import scipy.stats as stats
 import pylab
+from statsmodels.graphics.factorplots import interaction_plot
 
 
 df_consol_final = pd.DataFrame()
@@ -24,8 +25,7 @@ for compa in range(0,50):
     df['patient_number'] = df.index
     df_consol_final = df_consol_final.append(df)
     print("iteration number",compa)
-       
-df_consol_final.to_csv(r'/Users/gauravsharma/Documents/Simulation_results_4.csv') 
+        
 
 """
 plt.scatter(time_level_patients[:2],yield_harvesting[:2])
@@ -52,20 +52,12 @@ print('5_number_summary', quartiles)
 plt.show()
 
 plt.figure()
-Plot2 = df_consol_final.boxplot(by ='patient_number', column =['Achieved_Yield_from_Mfg'], grid= False, rot=90, fontsize=5)
+Plot1 = df_consol_final.boxplot(by ='run', column =['Achieved_Yield_from_Mfg'], grid= False, rot=90, fontsize=5)
 plt.show()
 
 plt.figure()
-Plot3 = df_consol_final.boxplot(by ='run', column =['Achieved_Yield_from_Mfg'], grid= False, rot=90, fontsize=5)
-plt.show()
-
-plt.figure()
-Plot4 = df_consol_final.boxplot(by ='patient_number', column =['diff_exp_achvd_yield_mfg'], grid= False, rot=90, fontsize=5)
-plt.show()
-
-plt.figure()
-Plot5 = df_consol_final.boxplot(by ='run', column =['diff_exp_achvd_yield_mfg'], grid= False, rot=90, fontsize=5)
-fig5 = Plot5.get_figure()
+Plot2 = df_consol_final.boxplot(by ='run', column =['diff_exp_achvd_yield_mfg'], grid= False, rot=90, fontsize=5)
+fig2 = Plot2.get_figure()
 plt.show()
 
 plt.figure()
@@ -75,8 +67,8 @@ current_palette = sns.color_palette("muted", n_colors=5)
 cmap = ListedColormap(sns.color_palette(current_palette).as_hex())
 N=90000
 colors = np.random.randint(0,5,N)
-Plot6 = plt.scatter(df_consol_final.patient_number, df_consol_final.diff_exp_achvd_yield_mfg, c=colors, cmap=cmap)
-fig6 = Plot6.get_figure()
+Plot3 = plt.scatter(df_consol_final.run, df_consol_final.Achieved_Yield_from_Mfg, c=colors, cmap=cmap)
+fig3 = Plot3.get_figure()
 plt.show()
 
 plt.figure()
@@ -86,7 +78,7 @@ current_palette = sns.color_palette("muted", n_colors=5)
 cmap = ListedColormap(sns.color_palette(current_palette).as_hex())
 N=90000
 colors = np.random.randint(0,5,N)
-Plot7 = plt.scatter(df_consol_final.run, df_consol_final.diff_exp_achvd_yield_mfg, c=colors, cmap=cmap)
+Plot4 = plt.scatter(df_consol_final.run, df_consol_final.diff_exp_achvd_yield_mfg, c=colors, cmap=cmap)
 plt.show()
 
 #plt.figure()
@@ -98,7 +90,7 @@ plt.show()
 
 
 plt.figure()
-Plot8 = df_consol_final.plot.hexbin(x='patient_number', y='Achieved_Yield_from_Mfg', gridsize=25)
+Plot5 = df_consol_final.plot.hexbin(x='run', y='Achieved_Yield_from_Mfg', gridsize=25)
 plt.show()
 
 #from pandas.plotting import scatter_matrix 
@@ -106,11 +98,14 @@ plt.show()
 
 plt.figure()
 sns.set_style("whitegrid") 
-Plot9 = sns.boxplot(x = 'patient_number', y = 'Achieved_Yield_from_Mfg', data = df_consol_final, width=0.5, palette="colorblind")
+Plot6 = sns.boxplot(x = 'run', y = 'Achieved_Yield_from_Mfg', data = df_consol_final, width=0.5, palette="colorblind")
 plt.show()
 
-Plot10 = stats.probplot(df_consol_final.Achieved_Yield_from_Mfg, dist="norm", plot=pylab)
-pylab.show()
+plt.figure()
+sns.set_style("whitegrid") 
+Plot7 = sns.boxplot(x = 'run', y = 'diff_exp_achvd_yield_mfg', data = df_consol_final, width=0.5, palette="colorblind")
+plt.show()
+
 
 Plot1.figure.savefig('Plot1.png')
 Plot2.figure.savefig('Plot2.png')
@@ -119,10 +114,89 @@ Plot4.figure.savefig('Plot4.png')
 Plot5.figure.savefig('Plot5.png')
 Plot6.figure.savefig('Plot6.png')
 Plot7.figure.savefig('Plot7.png')
-Plot8.figure.savefig('Plot8.png')
-Plot9.figure.savefig('Plot9.png')
+#Plot8.figure.savefig('Plot8.png')
+#Plot9.figure.savefig('Plot9.png')
 #Plot10.figure.savefig('Plot10.png')
 
+
+
+#DOE Plots
+
+DOE_Plot1 = stats.probplot(df_consol_final.Machine_Count, dist="norm", plot=pylab)
+pylab.show()
+
+DOE_Plot2 = stats.probplot(df_consol_final.Operators_Count, dist="norm", plot=pylab)
+pylab.show()
+
+DOE_Plot3 = stats.probplot(df_consol_final.Product_mix, dist="norm", plot=pylab)
+pylab.show()
+
+DOE_Plot4 = stats.probplot(df_consol_final.Achieved_Yield_from_Mfg, dist="norm", plot=pylab)
+pylab.show()
+
+DOE_Plot5 = stats.probplot(df_consol_final.diff_exp_achvd_yield_mfg, dist="norm", plot=pylab)
+pylab.show()
+
+DOE_Plot6_1 = df_consol_final.boxplot(by ='Machine_Count', column =['Achieved_Yield_from_Mfg'], grid= False, fontsize=5)
+plt.show()
+DOE_Plot6_2 = sns.boxplot(x = 'Machine_Count', y = 'Achieved_Yield_from_Mfg', data = df_consol_final, width=0.5, palette="colorblind")
+plt.show()
+
+DOE_Plot7_1 = df_consol_final.boxplot(by ='Operators_Count', column =['Achieved_Yield_from_Mfg'], grid= False, fontsize=5)
+plt.show()
+DOE_Plot7_2 = sns.boxplot(x = 'Operators_Count', y = 'Achieved_Yield_from_Mfg', data = df_consol_final, width=0.5, palette="colorblind")
+plt.show()
+
+DOE_Plot8_1 = df_consol_final.boxplot(by ='Product_mix', column =['Achieved_Yield_from_Mfg'], grid= False, fontsize=5)
+plt.show()
+DOE_Plot8_2 = sns.boxplot(x = 'Product_mix', y = 'Achieved_Yield_from_Mfg', data = df_consol_final, width=0.5, palette="colorblind")
+plt.show()
+
+DOE_Plot9 = interaction_plot(df_consol_final.Machine_Count,df_consol_final.Operators_Count, df_consol_final.Achieved_Yield_from_Mfg, ms=10)
+plt.show()
+
+DOE_Plot10 = interaction_plot(df_consol_final.Machine_Count,df_consol_final.Product_mix, df_consol_final.Achieved_Yield_from_Mfg, ms=10)
+plt.show()
+
+DOE_Plot11 = interaction_plot(df_consol_final.Operators_Count,df_consol_final.Product_mix, df_consol_final.Achieved_Yield_from_Mfg, ms=10)
+plt.show()
+
+#DOE_Plot6 = interaction_plot(df_consol_final.Operators_Count,df_consol_final.Achieved_Yield_from_Mfg, colors=['red','blue'], markers=['D','^'], ms=10)
+#plt.show()
+
+#DOE_Plot7 = interaction_plot(df_consol_final.Product_mix,df_consol_final.Achieved_Yield_from_Mfg, colors=['red','blue'], markers=['D','^'], ms=10)
+#plt.show()
+
+# Draw a pointplot to show response as a function of three categorical factors
+DOE_Plot12 = sns.catplot(x="Machine_Count", y="Achieved_Yield_from_Mfg", hue="Operators_Count", col="Product_mix",
+                capsize=.2, palette="husl", height=6, aspect=.75,
+                kind="point", data=df_consol_final)
+#DOE_Plot12.despine(left=True)
+plt.show()
+#DOE_Plot12.figure.savefig('DOE_Plot12.png')
+
+
+from pandas.tools import plotting
+DOE_Plot13 = plotting.scatter_matrix(df_consol_final[['Machine_Count', 'Operators_Count', 'Product_mix','Achieved_Yield_from_Mfg']], diagonal= 'kde')
+plt.show()
+
+from pandas.tools import plotting
+DOE_Plot13 = plotting.scatter_matrix(df_consol_final[['Machine_Count', 'Operators_Count', 'Product_mix','Achieved_Yield_from_Mfg']], diagonal= 'hist')
+plt.show()
+
+df_consol_final.to_csv(r'/Users/gauravsharma/Documents/Simulation_results_5.csv')
+
+#DOE_Plot14 = sns.residplot(df_consol_final[['Machine_Count', 'Operators_Count', 'Product_mix']], df_consol_final[['Achieved_Yield_from_Mfg']], lowess=True, color="g")
+#plt.show()
+
+
+#DOE_Plot1.figure.savefig('DOE_Plot1.png')
+#DOE_Plot2.figure.savefig('DOE_Plot2.png')
+#DOE_Plot3.figure.savefig('DOE_Plot3.png')
+#DOE_Plot4.figure.savefig('DOE_Plot4.png')
+#DOE_Plot5.figure.savefig('DOE_Plot5.png')
+#DOE_Plot6.figure.savefig('DOE_Plot6.png')
+#DOE_Plot7.figure.savefig('DOE_Plot7.png')
 
 #df_consol_final.to_excel(r'/Users/gauravsharma/Documents/Simulation_results_3.xls') 
 
